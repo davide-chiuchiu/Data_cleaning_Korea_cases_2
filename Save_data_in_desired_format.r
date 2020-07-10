a879a1e3d9ff85464c22b96620d68aadd5481c01
Davide Chiuchiù <dabol99@gmail.com>
2020-07-10 14:00:20 +0900

    Added code to save each province as a different file
    I have removed the nesting operation in favor of filtering to generate the data for each province and save them in separate files. I also have added a few lines to specify that the new files should be saved in a folder called provinces_files_folder located in the working directory.

--- Save_data_in_desired_format.r
+++ Save_data_in_desired_format.r
@@ -1,19 +1,39 @@
 library(tidyverse)
 library(data.table)
 
-
+# set working directory to the one containing this script
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# create folder containing script output 
+result_folder_name <- "provinces_files_folder"
+dir.create(paste(getwd(), '/', result_folder_name, sep = ''))
+
+# import the data from the csv dataset with korean prefecture data
 datafile_to_import <- "datasets_527325_1296546_TimeProvince.csv"
 Korea_cases <- fread(datafile_to_import) %>%
-  select(time = date, province, cases = confirmed, deaths = deceased, recovered = released)
+  mutate(hospitalized = NaN, icu = NaN) %>%
+  select(province, time = date, cases = confirmed, deaths = deceased, hospitalized, icu, recovered = released)
 
-Korea_cases_by_province <- Korea_cases %>%
-  nest(data = -c(province))
-
-for (i in 1 : nrow(Korea_cases_by_province)){
+# Create vector containing all Korean provinces
+Korea_provinces <- Korea_cases %>%
+  select(province) %>%
+  unique()
   
-  filename <- paste("KR-", Korea_cases_by_province[i,1], ".tsv", sep = '')
-  print(filename)
-  write.table(Korea_cases_by_province[i,2], file = filename, sep ='\t')
+# Create one file for each province containing the new infected people over time for that province
+for (i in 1 : nrow(Korea_provinces)){
+  
+  # Create filename from Korea prefecture and build header
+  filename <- paste(getwd(), '/', result_folder_name, '/', "KR-", Korea_provinces$province[i], ".tsv", sep = '')
+  cat('# Data Source: https://www.kaggle.com/kimjihoo/coronavirusdataset/data?select=TimeProvince.csv', '\n',  file = filename)
+  cat('# Data provenience: Korea Centers for Disease Control & Prevention', '\n',  file = filename, append = TRUE)
+  cat('# License: CC BY-NC-SA 4.0', '\n',  file = filename, append = TRUE)
+  
+  # save data from province as separate file
+  province_cases <- Korea_cases %>%
+    filter(province == Korea_provinces$province[i]) %>%
+    select(time, cases, deaths, hospitalized, icu, recovered) %>%
+    arrange(time)
+  
+  write.table(province_cases, file = filename, sep ='\t', append = TRUE, row.names = FALSE)
   
 }
